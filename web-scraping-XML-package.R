b7d2e422ea839705f28d3ad4a724d2a3ec6a72e5
triadicaxis <triadic.axis@gmail.com>
2018-05-24 03:38:55 +1000

    Added some new code for webscraping

--- web-scraping-XML-package.R
+++ web-scraping-XML-package.R
@@ -10,3 +10,12 @@ df2 <- readHTMLTable(url1, which=1, header=TRUE, stringsAsFactors=FALSE)
 ## Now view both dataframes
 df1
 df2
+
+## see also https://stackoverflow.com/questions/27192907/recursively-download-zip-files-from-webpage-windows#27192971
+
+pth <- "http://download.companieshouse.gov.uk/en_monthlyaccountsdata.html"
+library(XML)
+doc <- htmlParse(pth)
+myfiles <- doc["//a[contains(text(),'Accounts_Monthly_Data')]", fun = xmlAttrs]
+fileURLS <- file.path("http://download.companieshouse.gov.uk", myfiles)
+mapply(download.file, url = fileURLS, destfile = myfiles)
